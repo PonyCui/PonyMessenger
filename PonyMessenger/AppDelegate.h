04df1da9950ae67a619c90df070f6700e013720e
ponycui <PonyCui_Home@saitekiiMac.local>
2015-03-17 21:55:37 +0800

    Initial Commit

---
+++ PonyMessenger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PonyMessenger
+//
+//  Created by 崔 明辉 on 15-3-17.
+//  Copyright (c) 2015年 崔 明辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
