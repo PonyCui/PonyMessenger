553aaf23ecd35e93a89ed5ca912796e137b00523
ponycui <PonyCui_Home@saitekiiMac.local>
2015-03-18 23:26:31 +0800

    add Recent Cell

---
+++ PonyMessenger/Classes/Modules/Chat/User Interface/View/PPMChatRecentTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  PPMChatRecentTableViewCell.m
+//  PonyMessenger
+//
+//  Created by 崔 明辉 on 15-3-18.
+//  Copyright (c) 2015年 崔 明辉. All rights reserved.
+//
+
+#import "PPMChatRecentTableViewCell.h"
+
+@interface PPMChatRecentTableViewCell ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@end
+
+@implementation PPMChatRecentTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    // Configure the view for the selected state
+}
+
+@end
--- PonyMessenger/Classes/Modules/Chat/User Interface/View/PPMChatRecentViewController.m
+++ PonyMessenger/Classes/Modules/Chat/User Interface/View/PPMChatRecentViewController.m
@@ -8,7 +8,9 @@
 
 #import "PPMChatRecentViewController.h"
 
-@interface PPMChatRecentViewController ()
+@interface PPMChatRecentViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -26,14 +28,20 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UITableViewDataSource, UITableViewDelegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 99;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    return cell;
+}
 
 @end
